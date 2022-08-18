4e21364cc8f926f80da50f2e8d4ce9d3d3ad316c
Leandroswq <leandrobonfim15@hotmail.com>
2022-08-18 12:54:16 -0300

    feat: Criação da model matchModel

---
+++ app/backend/src/database/models/matchModel.ts
@@ -0,0 +1,50 @@
+import { Model, DataTypes } from 'sequelize';
+import db from './connection/tfc';
+
+class MatchModel extends Model {
+  id!: number;
+  homeTeam!: number;
+  homeTeamGoals!: number;
+  awayTeam!: number;
+  awayTeamGoals!: number;
+  inProgress!: boolean;
+
+  static associate(_model: object) { }
+}
+
+MatchModel.init({
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  homeTeam: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  homeTeamGoals: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  awayTeam: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  awayTeamGoals: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  inProgress: {
+    type: DataTypes.BOOLEAN,
+    allowNull: false,
+  },
+}, {
+  sequelize: db,
+  modelName: 'MatchModel',
+  tableName: 'matches',
+  underscored: true,
+  timestamps: false,
+});
+
+export default MatchModel;
